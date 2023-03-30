f5e18571cefbcbf8d53d7b9763f99189c240c2a7
unknown <matthewajustice@gmail.com>
2023-03-30 16:24:36 -0400

    fixed import that was breaking deployment

--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
-export { default as ThreeBox } from './Threebox';
+export { default as ThreeBox } from './ThreeBox';
